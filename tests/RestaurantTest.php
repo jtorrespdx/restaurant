ea794c26bc6403fda0c47463ed9432c85017135b
nathan young & jose torres <student@epicodus.com>
2015-08-19 12:16:19 -0700

    adds remaining tests to restauranttest.php

--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -86,21 +86,6 @@
             $this->assertEquals($test_restaurant, $result[0]);
         }
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         function test_getAll()
         {
             //arrange
@@ -128,6 +113,65 @@
 
             //assert
             $this->assertEquals([$test_name, $test_name2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //arrange
+            $type = "Tacos";
+            $id = null;
+            $test_cuisine = new Cuisine($type, $id);
+            $test_cuisine->save();
+
+            $name = "Nathan's";
+            $cuisine_id = $test_cuisine->getId();
+            $price_range = 1;
+            $neighborhood = "Felony Flats";
+            $test_restaurant = new Restaurant($name, $id, $cuisine_id, $price_range, $neighborhood);
+            $test_restaurant->save();
+
+            $name2 = "Jose's";
+            $price_range = 2;
+            $neighborhood = "Buckman";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant2 = new Restaurant($name2, $id, $cuisine_id, $price_range, $neighboorhood);
+            $test_restaurant2->save();
+
+            //act
+            Restaurant::deleteAll();
+
+            //assert
+            $result = Restaurant::getAll();
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            //arrange
+            $type = "Tacos";
+            $id = null;
+            $test_cuisine = new Cuisine($type, $id);
+            $test_cuisine->save();
+
+            $name = "Nathan's";
+            $cuisine_id = $test_cuisine->getId();
+            $price_range = 1;
+            $neighborhood = "Felony Flats";
+            $test_restaurant = new Restaurant($name, $id, $cuisine_id, $price_range, $neighborhood);
+            $test_restaurant->save();
+
+            $name2 = "Jose's";
+            $price_range = 2;
+            $neighborhood = "Buckman";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant2 = new Restaurant($name2, $id, $cuisine_id, $price_range, $neighboorhood);
+            $test_restaurant2->save();
+
+            //act
+            $result = Restaurant::find($test_restaurant2->getId());
+
+            //assert
+            $this->assertEquals($test_restaurant2, $result);
         }
 
 
